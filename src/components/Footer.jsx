bf8e93669c05452c3bd34ec4fda30575d38130f3
chems eddine bourabia <bourabiachemseddine@gmail.COM>
2024-10-30 23:21:58 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,21 +43,19 @@ export default function Footer() {
           </nav>
 
           <div className="flex space-x-4 items-center" id="footer">
-            <Link href="https://www.instagram.com/brahim_mrz_store/" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
-              <FaInstagram className={`fill-orange-500 hover:fill-gray-400 h-6 w-6 ${twitch ? 'icon-twitch' : ''}`} />
-            </Link>
-            <Link href="mailto:meharzibr@gmail.com" aria-label="Email" target="_blank" rel="noopener noreferrer">
-              <FaEnvelope className={`fill-blue-500 hover:fill-gray-400 h-6 w-6 ${twitch ? 'icon-twitch' : ''}`} />
-            </Link>
-            <Link href="tel:0791084298" aria-label="Phone" target="_blank" rel="noopener noreferrer">
-              <FaPhone className={`fill-green-500 hover:fill-gray-400 h-6 w-6 ${twitch ? 'icon-twitch' : ''}`} />
-            </Link>
-            <Link href="https://wa.me/0540207506" aria-label="WhatsApp" target="_blank" rel="noopener noreferrer">
-              <FaWhatsapp className={`fill-green-500 hover:fill-gray-400 h-6 w-6 ${twitch ? 'icon-twitch' : ''}`} />
-            </Link>
-          </div>
-        </div>
-
+    <Link href="https://www.instagram.com/brahim_mrz_store/" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
+        <FaInstagram className={`fill-orange-500 hover:fill-gray-400 h-6 w-6 ${twitch ? 'icon-twitch' : ''}`} />
+    </Link>
+    <Link href="mailto:meharzibr@gmail.com" aria-label="Email" target="_blank" rel="noopener noreferrer">
+        <FaEnvelope className={`fill-blue-500 hover:fill-gray-400 h-6 w-6 ${twitch ? 'icon-twitch' : ''}`} />
+    </Link>
+    <Link href="tel:0791084298" aria-label="Phone" target="_blank" rel="noopener noreferrer">
+        <FaPhone className={`fill-green-500 hover:fill-gray-400 h-6 w-6 ${twitch ? 'icon-twitch' : ''}`} />
+    </Link>
+    <Link href="https://wa.me/213540207506" aria-label="WhatsApp" target="_blank" rel="noopener noreferrer">
+        <FaWhatsapp className={`fill-green-500 hover:fill-gray-400 h-6 w-6 ${twitch ? 'icon-twitch' : ''}`} />
+    </Link>
+</div>
         <div className="border-t border-gray-700 my-4"></div>
         <p className="text-center text-sm text-gray-500">
           &copy; {new Date().getFullYear()} Embroidery DZ for Embroidery Clothing. All rights reserved.
