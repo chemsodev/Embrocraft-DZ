72d28e940ec7e1df06a43aa8d6928bed1d2abb4b
chemso <bourabiachemseddine@gmail.com>
2024-10-30 13:42:04 +0100

    added logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,15 +3,22 @@ import "./globals.css";
 
 export const metadata = {
   title: "Embroidery DZ",
-  description: "Discover your perfect custom embroidered clothing that reflects your unique style and personality. Create, design, and order one-of-a-kind pieces with ease!"
+  description: "Discover your perfect custom embroidered clothing that reflects your unique style and personality. Create, design, and order one-of-a-kind pieces with ease!",
+  openGraph: {
+    images: "/images/logo.jpg"
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <head>
+      <link rel="icon" href="/images/logo.jpg" type="image/jpeg" />
+    </head>
+    <body>
+      {children}
+    </body>
+  </html>
+
   );
 }
