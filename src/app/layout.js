b7da977f9185bb9a967c98545b5f89e5f8251ae0
chemso <bourabiachemseddine@gmail.com>
2024-11-07 13:22:21 +0100

    changed name

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,19 +9,19 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/images/Logo.png" type="image/png" />
         <link rel="preload" href="/images/Logo.jpg" as="image" />
-        <title>تطريز DZ</title>
+        <title>Embrocraft DZ</title>
         <meta
           name="description"
           content="اكتشف الملابس المطرزة المخصصة التي تعكس أسلوبك وشخصيتك الفريدة. أنشئ وصمم واطلب قطعًا فريدة من نوعها بسهولة!"
         />
-        <meta property="og:url" content="https://embroidery-dz.vercel.app/" />
+        <meta property="og:url" content="https://embrocraft-dz.vercel.app/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="تطريز DZ" />
         <meta
           property="og:description"
           content="اكتشف الملابس المطرزة المخصصة التي تعكس أسلوبك وشخصيتك الفريدة. أنشئ وصمم واطلب قطعًا فريدة من نوعها بسهولة!"
         />
-        <meta property="og:image" content="https://embroidery-dz.vercel.app/images/Logo.jpg" />
+        <meta property="og:image" content="https://embrocraft-dz.vercel.app/images/Logo.jpg" />
       </head>
       <body>
         <Header />
